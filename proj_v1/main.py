e28295b2f6b6fb9018c7bd02281e8ed370545c44
sorin-albu <sorin.albu@student.tuiasi.ro>
2026-01-19 14:13:41 +0200

    schimbare interfata la custom tkinter si statistics

--- proj_v1/main.py
+++ proj_v1/main.py
@@ -1,11 +1,13 @@
-# main.py
-import tkinter as tk
-from tkinter import ttk
+
+import customtkinter as ctk 
 from database import db_config
 from ml_logic import MLHandler
 import gui_frames as gui
 
-class App(tk.Tk):
+ctk.set_appearance_mode("System") 
+ctk.set_default_color_theme("blue")
+
+class App(ctk.CTk): 
     def __init__(self):
         super().__init__()
         self.title("Sistem Suport Decizional - Liver Disease 2026")
@@ -15,16 +17,17 @@ class App(tk.Tk):
         self.logged_user_id = None
         self.logged_user_role = None
         self.test_sizes = [0.20, 0.30, 0.40, 0.50]
-        
-        # Initializare ML din fisierul ml_logic.py
         self.ml_handler = MLHandler(self.test_sizes)
         self.models_data = self.ml_handler.initialize_ml_logic()
 
-        container = ttk.Frame(self)
+        container = ctk.CTkFrame(self)
         container.pack(fill="both", expand=True)
+        
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
         self.frames = {}
 
-        # Incarcare cadre din gui_frames.py
         for F in (gui.LoginFrame, gui.DashboardFrame, gui.PatientFormFrame, 
                   gui.PredictionFrame, gui.StatisticsFrame, gui.AdminUserFrame, gui.HistoryFrame):
             frame = F(parent=container, controller=self)
